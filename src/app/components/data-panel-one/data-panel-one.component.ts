7de1875a44b99208b02c8922be2b8e78e18f5f7f
Nick Romero <nromero@protonmail.com>
2018-05-08 13:24:50 -0400

    defines margins, groups, axes, and labels

--- src/app/components/data-panel-one/data-panel-one.component.ts
+++ src/app/components/data-panel-one/data-panel-one.component.ts
@@ -25,73 +25,110 @@ export class DataPanelOneComponent implements OnInit {
 
     this.buildings = this.buildingService.getBuildings();
 
-    /* original concept was to consume json data through
-       a RESTful API using Angular 5 service patterns
-       (i.e. RxJS > Observable > Subscribe)
-
-       d3.json will tap an external source
-       using a javascript/ES6 promise w/ arrow func
-       not sure about performance implications with either approach */
-
-    // mock service up and running
+    // call in mock service
     console.log(this.buildings);
 
-    // define the canvas area via DOM selector
-    const svg = d3.select('#chart-area')
+    /* set window rules for visualization, deduct
+       margins from h/w vars to plug them in w/out nudging */
+    const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    /* define a group as g, shift the group
+       around inside the selected DOM area */
+    const g = d3.select('#chart-area')
       .append('svg')
-        .attr('width', '500')
-        .attr('height', '600');
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+          .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+    // configure the X Label
+    g.append('text')
+      .attr('class', 'x axis-label')
+      .attr('x', width / 2)
+      .attr('y', height + 140)
+      .attr('font-size', '22px')
+      .attr('text-anchor', 'middle')
+      .text('The world\'s tallest buildings');
+
+    // configure the Y Label
+    g.append('text')
+      .attr('class', 'y axis-label')
+      .attr('x', - (height / 2))
+      .attr('y', -60)
+      .attr('font-size', '22px')
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)')
+      .text('Height in Meters');
 
     // pull static data from the assets dir
-    // latest version of d3 affords use of js promises to build visuals
     d3.json('../../../assets/static-data/buildings.json').then((data) => {
       console.log(data);
 
+      /* define data as d, iterate over
+         and create an index for access */
       data.forEach(d => {
-        console.log(d.height);
         d.height = +d.height;
       });
 
-      // height proportioned to height of buildings
       const x = d3.scaleBand()
-                // maps the defined array of data
+                // maps/calls the defined array of data
                 .domain(data.map((d) => {
                   return d.name;
                 }))
-                .range([0, 400])
-                .paddingInner(0.4)
-                .paddingOuter(0.4);
-
-      console.log(x('Burj Khalifa'));
+                .range([0, width])
+                .paddingInner(0.3)
+                .paddingOuter(0.3);
 
       const y = d3.scaleLinear()
                   .domain([0, d3.max(data, (d) => {
                     return d.height;
                   })])
-                  .range([0, 400]);
+                  .range([0, height]);
 
-      const rects = svg.selectAll('rect')
-                     .data(data)
-                     .enter()
-                     .append('rect')
-                     .attr('y', 20)
-                     .attr('x', (d) => {
-                     // .attr('x', (d, i) => {
-                       // return (i * 60);
-                       return x(d.name);
-                     })
-                     // .attr('width', 40)
-                     .attr('width', x.bandwidth)
-                     .attr('height', (d) => {
-                       return y(d.height);
-                     })
-                     .attr('fill', (d) => {
-                       return 'grey';
-                     });
+      // axis graph/line text generators
+      const xAxisCall = d3.axisBottom(x);
+      // define the group for xAxis
+      g.append('g')
+       .attr('class', 'x axis')
+       .attr('transform', 'translate(0, ' + height + ')')
+       .call(xAxisCall)
+       .selectAll('text') // alter text labels
+         .attr('y', '10')
+         .attr('x', '-5')
+         .attr('text-anchor', 'end')
+         .attr('transform', 'rotate(-40)');
+
+      const yAxisCall = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat((d) => {
+          return d + 'm'; // format in meters
+        });
+      // define the group for yAxis
+      g.append('g')
+        .attr('class', 'y-axis')
+        .call(yAxisCall);
+
+      const rects = g.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('y', 0)
+        .attr('x', (d) => {
+          return x(d.name);
+        })
+        .attr('width', x.bandwidth)
+        .attr('height', (d) => {
+          return y(d.height);
+        })
+        .attr('fill', (d) => {
+          return 'grey';
+        });
 
     }).catch((error) => {
-      // new - we can catch errors when promises are rejected
-      console.log(error);
+        console.log(error);
     });
 
   } // ngOnInit
